f95a4fcb5cb2b31be5a5fa6cea6ac46e1c28bac6
Sebas5158R <sebastianriveraa4@gmail.com>
2026-05-01 10:50:17 -0500

    refactor(prisma): move Prisma client initialization to a separate utility file

--- backend/modules/usuarios/usuario.service.js
+++ backend/modules/usuarios/usuario.service.js
@@ -1,9 +1,4 @@
-import "dotenv/config";
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../../generated/prisma/index.js";
-
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
-const prisma = new PrismaClient({ adapter });
+import prisma from "../../utils/prisma.js";
 
 export const getUsuarios = async () => {
     return prisma.usuarios.findMany();
--- backend/tests/test.js
+++ backend/tests/test.js
@@ -1,13 +1,7 @@
 // test.js
-import "dotenv/config"
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../generated/prisma/index.js";
-
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
-const prisma = new PrismaClient({ adapter });
+import prisma from "../utils/prisma.js";
 
 async function main() {
-  console.log(process.env.DATABASE_URL);
   const usuarios = await prisma.usuarios.findMany();
   console.log(usuarios);
 }
